acdb009c02e3db3474a57eee19e6fb7af7590552
jiashen <1131621528@qq.com>
2021-01-05 01:01:17 +0800

    Update BingWallpapers.js

--- BingWallpapers.js
+++ BingWallpapers.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        百度/谷歌首页调用bing壁纸
+// @name         百度/谷歌首页调用bing壁纸
 // @namespace    https://github.com/jiashende/BingWallpapers/blob/master/BingWallpapers.js
 // @version      0.2
 // @description  搬运了别人的Jscript，加以修改
